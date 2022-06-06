e6d6553cb2a49b97a655b9a704e1e13ea190d8bb
mahath <mahathmu@outlook.com>
2022-06-07 00:02:12 +0530

    social meadia link add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './component/Home'
+import About from './component/About'
+import Footer from './component/Footer'
+import colors from './Assets/Colours.json'
+import Header from './component/Header'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header className="header"
+        LogoColor = {colors.DarkBlue}
+      />
+      <Home className="home"
+        BgColor = {colors.greywhite}
+        FontColor = {colors.DarkBlue}
+        objColor ={colors.Blue}
+        LiteColor = {colors.LightBlue}
+      />
+
+      <Footer className="footer"
+        IconColor ={colors.DarkBlue} 
+        OnClickColor = {colors.grey} 
+        ShadowColor ={colors.grey} 
+        TabColor ={colors.greywhite} 
+      />
+      <About 
+          BgColor = {colors.greywhite}
+          FontColor = {colors.DarkBlue}
+          objColor ={colors.Blue}
+          LiteColor = {colors.LightBlue}
+      />
     </div>
   );
 }
