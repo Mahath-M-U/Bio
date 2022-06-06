9e64d1261bef9a51b0d362d492e13dbd191339e7
mahath <mahathmu@outlook.com>
2022-06-07 01:18:17 +0530

    home page navigation and rout

--- src/App.js
+++ src/App.js
@@ -2,35 +2,56 @@ import './App.css';
 import Home from './component/Home'
 import About from './component/About'
 import Footer from './component/Footer'
-import colors from './Assets/Colours.json'
 import Header from './component/Header'
+import colors from './Assets/Colours.json'
+import {Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="app">
-      <Header className="header"
-        LogoColor = {colors.DarkBlue}
-      />
-      <Home className="home"
-        BgColor = {colors.greywhite}
-        FontColor = {colors.DarkBlue}
-        objColor ={colors.Blue}
-        LiteColor = {colors.LightBlue}
-      />
+    <Header className="header" LogoColor = {colors.DarkBlue}/>
+      <Routes>
+      <Route path="*" element={<h1 style={{
+        width:'100vw',
+        height:'100vh',
+        display:'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
 
-      <Footer className="footer"
-        IconColor ={colors.DarkBlue} 
-        OnClickColor = {colors.grey} 
-        ShadowColor ={colors.grey} 
-        TabColor ={colors.greywhite} 
-      />
-      <About 
-          BgColor = {colors.greywhite}
-          FontColor = {colors.DarkBlue}
-          objColor ={colors.Blue}
-          LiteColor = {colors.LightBlue}
-      />
+        }}>NO Page</h1>} />
+          <Route path="/" element={
+            <Home className="home"
+              BgColor = {colors.greywhite}
+              FontColor = {colors.DarkBlue}
+              objColor ={colors.Blue}
+              LiteColor = {colors.LightBlue} />}/>
+          <Route path="/about" element={
+            <About 
+                BgColor = {colors.greywhite}
+                FontColor = {colors.DarkBlue}
+                objColor ={colors.Blue}
+                LiteColor = {colors.LightBlue} />}/>
+          <Route path="/works" element={<h1 style={{
+            width:'100vw',
+            height:'100vh',
+            display:'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+            }}>Coming Soon</h1>} />
+          <Route path="/services" element={<h1 style={{
+            width:'100vw',
+            height:'100vh',
+            display:'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+            }}>Coming Soon</h1>} />
+        </Routes>
+       <Footer className="footer"
+              IconColor ={colors.DarkBlue} 
+              OnClickColor = {colors.grey} 
+              ShadowColor ={colors.grey} 
+              TabColor ={colors.greywhite} />
     </div>
   );
 }
